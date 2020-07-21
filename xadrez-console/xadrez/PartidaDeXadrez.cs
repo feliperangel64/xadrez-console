7db96914853a99d845781bca6604460a8d3ba245
Felipe Rangel <felipe.rangel.64@gmail.com>
2020-07-21 20:17:53 -0300

    Jogada especial promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -253,6 +253,21 @@ namespace xadrez
         {
             Peca capturada = executaMovimento(origem, destino);
 
+            Peca p = tab.peca(destino);
+
+            // #jogada especial promocao
+            if (p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(jogadorAtual))
             {
                 desfazMovimento(origem, destino, capturada);
@@ -277,8 +292,6 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino);
 
             //jogada especial en passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
